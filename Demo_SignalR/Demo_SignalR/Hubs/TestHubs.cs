67041eae6410c6e09c756cb44ac309f084f403f0
Leo <e23882@hotmail.com>
2020-03-10 15:41:55 +0800

    first version commit

---
+++ Demo_SignalR/Demo_SignalR/Hubs/TestHubs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Demo_SignalR.Hubs
+{
+    public class TestHubs : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the addNewMessageToPage method to update clients.
+            Clients.All.addNewMessageToPage(name, message);
+        }
+    }
+}
